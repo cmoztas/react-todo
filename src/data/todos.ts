98bf00c0b2b5c05d8f5919be3a7f493568be4e52
Cagkan <cmoztas2@gmail.com>
2024-02-20 23:50:43 +0300

    rendering todos with map

---
+++ src/data/todos.ts
@@ -0,0 +1,19 @@
+import {Todo} from "../types/todo.ts";
+
+export const dummyData: Todo[] = [
+    {
+        id: 1,
+        title: 'Give a star this project on GitHub 👍🏻',
+        completed: false
+    },
+    {
+        id: 2,
+        title: 'Add me on LinkedIn 🥰',
+        completed: false
+    },
+    {
+        id: 3,
+        title: 'Send a nice message 😊',
+        completed: false
+    }
+];
